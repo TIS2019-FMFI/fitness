eb68db3b56e1a40c6f1383cdbabd440446ffd0f5
Denis <capkovicdenis@gmail.com>
2019-10-24 10:15:37 +0200

    Add prettier config and apply it

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import snakeoil from "./images/snakeoil.jpg";
-import "./App.scss";
+import React from 'react';
+import snakeoil from './images/snakeoil.jpg';
+import './App.scss';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={snakeoil} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={snakeoil} className='App-logo' alt='logo' />
+            </header>
+        </div>
+    );
 };
 
 export default App;
