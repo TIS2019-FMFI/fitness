479208de43f39b92468f92729149d822a36d5369
Denis <capkovicdenis@gmail.com>
2019-10-26 20:16:19 +0200

    [#4] Add basic routing to app
    After many tries...

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,33 +1,70 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import snakeoil from './images/snakeoil.jpg';
-
-const AppHeader = styled.div`
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-`;
-
-const Logo = styled.img`
-    height: 40vmin;
-`;
-
-export interface Props {}
-
-class App extends React.Component<Props> {
-    render() {
-        return (
-            <AppHeader>
-                <Logo src={snakeoil} alt='logo' />
-            </AppHeader>
-        );
-    }
+function App() {
+    return (
+        <Router>
+            <Container>
+                <Menu>
+                    <Route>
+                        {({ location }) => (
+                            <React.Fragment>
+                                <MenuLink
+                                    to='/'
+                                    selected={location.pathname === '/' || location.pathname === '/reservation'}
+                                >
+                                    <span>Rezervacie</span>
+                                </MenuLink>
+                                <MenuLink to='/history' selected={location.pathname === '/history'}>
+                                    <span>Historia objednavok</span>
+                                </MenuLink>
+                                <MenuLink to='/users' selected={location.pathname === '/users'}>
+                                    <span>Sprava klientov</span>
+                                </MenuLink>
+                                <MenuLink to='/procedures' selected={location.pathname === '/procedures'}>
+                                    <span>Stroje a procedury</span>
+                                </MenuLink>
+                            </React.Fragment>
+                        )}
+                    </Route>
+                </Menu>
+                <Switch>
+                    <Route path='/users'>
+                        <p>Users</p>
+                    </Route>
+                    <Route path='/reservation'>
+                        <p>reservation</p>
+                    </Route>
+                    <Route path='/procedures'>
+                        <p>procedures</p>
+                    </Route>
+                    <Route path='/history'>
+                        <p>history</p>
+                    </Route>
+                    <Route path='/'>
+                        <p>Main</p>
+                    </Route>
+                </Switch>
+            </Container>
+        </Router>
+    );
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-flow: row;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-flow: column;
+`;
+
+const MenuLink = styled(Link)`
+    color: Blue;
+    text-decoration: none;
+    background-color: ${props => (props.selected ? 'red' : 'unset')};
+`;
 
 export default App;
