1ee135b55833ce4bf69e78ed4962dc96fec1e099
babalsim <simon.babal@outlook.com>
2019-11-23 21:38:29 +0100

    added empty requests and updated ClientsController.php

--- src/backend/app/Http/Controllers/Api/v1/ClientsController.php
+++ src/backend/app/Http/Controllers/Api/v1/ClientsController.php
@@ -3,32 +3,54 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\DestroyClient;
+use App\Http\Requests\EditClient;
+use App\Http\Requests\IndexClient;
+use App\Http\Requests\StoreClient;
+use App\Http\Requests\UpdateClient;
+use App\Models\Client;
+use Illuminate\Http\Response;
 
 class ClientsController extends Controller
 {
     //TODO: implement me pls
-    public function index(){}
+    public function index(/*IndexClient $request*/) : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function create(){}
+    public function create() : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function history(){}
+    public function history(/*IndexClient $request*/) : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function store(){}
+    public function store(/*StoreClient $request*/) : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function edit(){}
+    public function edit(/*EditClient $request, Client $client*/) : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function update(){}
+    public function update(/*UpdateClient $request, Client $client*/) : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function destroy(){}
+    public function destroy(/*DestroyClient $request, Client $client*/) : Response{
+        return Response::create();
+    }
 
     //TODO: implement me pls
-    public function findClient(){}
+    public function findClient($id) : Client{
+        return Client::with()->find($id);
+    }
 
 }
