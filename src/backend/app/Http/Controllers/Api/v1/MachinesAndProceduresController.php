ae2e1fb468ef289191799268cda5b3a3cff7c84e
Richard <riso.dominik@gmail.com>
2019-11-25 21:59:43 +0100

    request as parameters in MachinesAndProceduresController

--- src/backend/app/Http/Controllers/Api/v1/MachinesAndProceduresController.php
+++ src/backend/app/Http/Controllers/Api/v1/MachinesAndProceduresController.php
@@ -3,28 +3,36 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\DestroyMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\IndexMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\ShowMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\StoreMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\UpdateMachinesAndProcedure;
+
 
 class MachinesAndProceduresController extends Controller
 {
-    //TODO: implement me pls
-    public function index(){}
 
-    //TODO: implement me pls
-    public function create(){}
+    public function index(IndexMachinesAndProcedure $request) {
 
-    //TODO: implement me pls
-    public function history(){}
+    }
 
-    //TODO: implement me pls
-    public function store(){}
+    public function showHistory(IndexMachinesAndProcedure $request) {
 
-    //TODO: implement me pls
-    public function edit(){}
+    }
 
-    //TODO: implement me pls
-    public function update(){}
+    public function store(StoreMachinesAndProcedure $request) {
 
-    //TODO: implement me pls
-    public function destroy(){}
+    }
+
+    //TODO implement me pls
+    public function show() {}
+
+    public function update(UpdateMachinesAndProcedure $request) {
+
+    }
+
+    public function destroy(DestroyMachinesAndProcedure $request) {
+
+    }
 }
