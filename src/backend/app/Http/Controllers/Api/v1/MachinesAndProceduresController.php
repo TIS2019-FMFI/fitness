272674f00b809169802daf769ab149390240f05c
Richard <riso.dominik@gmail.com>
2019-11-25 23:07:30 +0100

    deleted create routes, modified routes ids, MachinesAndProcedures controllers methods in progress

--- src/backend/app/Http/Controllers/Api/v1/MachinesAndProceduresController.php
+++ src/backend/app/Http/Controllers/Api/v1/MachinesAndProceduresController.php
@@ -5,34 +5,45 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\MachinesAndProcedures\DestroyMachinesAndProcedure;
 use App\Http\Requests\Api\v1\MachinesAndProcedures\IndexMachinesAndProcedure;
-use App\Http\Requests\Api\v1\MachinesAndProcedures\ShowMachinesAndProcedure;
 use App\Http\Requests\Api\v1\MachinesAndProcedures\StoreMachinesAndProcedure;
 use App\Http\Requests\Api\v1\MachinesAndProcedures\UpdateMachinesAndProcedure;
-
+use App\Models\MachinesAndProcedure;
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
 
 class MachinesAndProceduresController extends Controller
 {
 
-    public function index(IndexMachinesAndProcedure $request) {
-
+    public function index(IndexMachinesAndProcedure $request): Collection {
+        return MachinesAndProcedure::all();
     }
 
     public function showHistory(IndexMachinesAndProcedure $request) {
 
     }
 
-    public function store(StoreMachinesAndProcedure $request) {
+    public function store(StoreMachinesAndProcedure $request): Response {
+        $sanitized = $request->validated();
+        $machinesAndProcedure = MachinesAndProcedure::create($sanitized);
 
+        return response()->json($machinesAndProcedure, 201);
     }
 
     //TODO implement me pls
     public function show() {}
 
-    public function update(UpdateMachinesAndProcedure $request) {
+    public function update(UpdateMachinesAndProcedure $request, int $id): Response {
+        $machinesAndProcedure = MachinesAndProcedure::findOrFail($id);
+        $data = $request->validated();
+        $machinesAndProcedure->update($data);
 
+        return response()->json($machinesAndProcedure, 200);
     }
 
-    public function destroy(DestroyMachinesAndProcedure $request) {
+    public function destroy(DestroyMachinesAndProcedure $request, int $id): Response {
+        $machinesAndProcedure = MachinesAndProcedure::findOrFail($id);
+        $machinesAndProcedure->delete();
 
+        return response()->json(null, 204);
     }
 }
--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -24,32 +24,29 @@ Route::prefix('v1')->namespace('Api\v1')->group(static function () {
 
     Route::prefix('clients')->group(static function() {
         Route::get('/',                                     'ClientsController@index');
-        Route::get('/create',                               'ClientsController@create');
         Route::get('/history',                              'ClientsController@history');
         Route::post('/',                                    'ClientsController@store');
-        Route::get('/{client}/edit',                        'ClientsController@edit')->name('edit');
-        Route::post('/{client}',                            'ClientsController@update')->name('update');
-        Route::delete('{client}',                           'ClientsController@destroy')->name('destroy');
+        Route::get('/{clientId}/edit',                        'ClientsController@edit')->name('edit');
+        Route::post('/{clientId}',                            'ClientsController@update')->name('update');
+        Route::delete('{clientId}',                           'ClientsController@destroy')->name('destroy');
         Route::get('/findClient/{query?}',                  'ClientsController@findClient')->name('findClient');
     });
 
     Route::prefix('machines-and-procedures')->group(static function() {
         Route::get('/',                                     'MachinesAndProceduresController@index');
-        Route::get('/create',                               'MachinesAndProceduresController@create');
         Route::get('/history',                              'MachinesAndProceduresController@history');
         Route::post('/',                                    'MachinesAndProceduresController@store');
-        Route::get('/{machinesAndProcedure}/edit',          'MachinesAndProceduresController@edit')->name('edit');
-        Route::post('/{machinesAndProcedure}',              'MachinesAndProceduresController@update')->name('update');
-        Route::delete('{machinesAndProcedure}',             'MachinesAndProceduresController@destroy')->name('destroy');
+        Route::get('/{machinesAndProcedureId}/edit',          'MachinesAndProceduresController@edit')->name('edit');
+        Route::post('/{machinesAndProcedureId}',              'MachinesAndProceduresController@update')->name('update');
+        Route::delete('{machinesAndProcedureId}',             'MachinesAndProceduresController@destroy')->name('destroy');
     });
 
     Route::prefix('orders')->group(static function() {
         Route::get('/',                                     'OrdersController@index');
-        Route::get('/create',                               'OrdersController@create');
         Route::post('/',                                    'OrdersController@store');
-        Route::get('/{order}/edit',                         'OrdersController@edit')->name('edit');
-        Route::post('/{order}',                             'OrdersController@update')->name('update');
-        Route::delete('{order}',                            'OrdersController@destroy')->name('destroy');
+        Route::get('/{orderId}/edit',                         'OrdersController@edit')->name('edit');
+        Route::post('/{orderId}',                             'OrdersController@update')->name('update');
+        Route::delete('{orderId}',                            'OrdersController@destroy')->name('destroy');
         Route::get('/findOrder/{query?}',                   'OrdersController@findOrder')->name('findOrder');
     });
 
