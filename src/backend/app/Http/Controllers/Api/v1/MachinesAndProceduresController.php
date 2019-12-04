ea8ca46b21a9d69c413803d9123316164b8082ae
babalsim <simon.babal@outlook.com>
2019-12-04 23:11:21 +0100

    fixed conflict in MachinesAndProceduresController.php

--- src/backend/app/Http/Controllers/Api/v1/MachinesAndProceduresController.php
+++ src/backend/app/Http/Controllers/Api/v1/MachinesAndProceduresController.php
@@ -1,20 +1,15 @@
 <?php
-
 namespace App\Http\Controllers\Api\v1;
-
 use App\Http\Controllers\Controller;
-
-use App\Http\Requests\Api\v1\MachinesAndProcedure\DestroyMachinesAndProcedure;
-use App\Http\Requests\Api\v1\MachinesAndProcedure\IndexMachinesAndProcedure;
-use App\Http\Requests\Api\v1\MachinesAndProcedure\StoreMachinesAndProcedure;
-use App\Http\Requests\Api\v1\MachinesAndProcedure\UpdateMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\DestroyMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\IndexMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\StoreMachinesAndProcedure;
+use App\Http\Requests\Api\v1\MachinesAndProcedures\UpdateMachinesAndProcedure;
 use App\Models\MachinesAndProcedure;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Collection;
-
 class MachinesAndProceduresController extends Controller
 {
-
     /**
      * Display data of the resource.
      *
@@ -24,18 +19,15 @@ class MachinesAndProceduresController extends Controller
     public function index(IndexMachinesAndProcedure $request): Collection {
         return MachinesAndProcedure::all();
     }
-
     /**
      * Display the specified resource history.
      *
      * @param IndexMachinesAndProcedure $request
-     * @return Collection
+     * @return void
      */
-    public function showHistory(IndexMachinesAndProcedure $request): Collection {
-        $orders = Order::with('orders')->get();
-        return $orders;
+    public function showHistory(IndexMachinesAndProcedure $request) {
+        //TODO implement me pls
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -45,11 +37,8 @@ class MachinesAndProceduresController extends Controller
     public function store(StoreMachinesAndProcedure $request): JsonResponse {
         $sanitized = $request->validated();
         $machinesAndProcedure = MachinesAndProcedure::create($sanitized);
-
         return response()->json($machinesAndProcedure, 201);
     }
-
-
     /**
      * Display the specified resource.
      *
@@ -58,7 +47,6 @@ class MachinesAndProceduresController extends Controller
     public function show() {
         //TODO implement me pls
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -70,10 +58,8 @@ class MachinesAndProceduresController extends Controller
         $machinesAndProcedure = MachinesAndProcedure::findOrFail($machinesAndProcedureId);
         $data = $request->validated();
         $machinesAndProcedure->update($data);
-
         return response()->json($machinesAndProcedure, 200);
     }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -84,7 +70,6 @@ class MachinesAndProceduresController extends Controller
     public function destroy(DestroyMachinesAndProcedure $request, int $machinesAndProcedureId): JsonResponse {
         $machinesAndProcedure = MachinesAndProcedure::findOrFail($machinesAndProcedureId);
         $machinesAndProcedure->delete();
-
         return response()->json(null, 204);
     }
 }
