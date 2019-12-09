0ce972f40c8a848f69796f0a3866e35fd2c71985
Richard <riso.dominik@gmail.com>
2019-12-09 23:25:42 +0100

    jwt auth in progress

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['middleware' => 'api-header'], function () {
+
+    // The registration and login requests doesn't come with tokens
+    // as users at that point have not been authenticated yet
+    // Therefore the jwtMiddleware will be exclusive of them
+    Route::post('user/login', 'UserController@login');
+    Route::post('user/register', 'UserController@register');
 });
