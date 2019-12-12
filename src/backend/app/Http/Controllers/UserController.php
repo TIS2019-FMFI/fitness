f2609fe28710ea9c7131abb06771bead5c5deb3d
Richard <riso.dominik@gmail.com>
2019-12-12 14:37:33 +0100

    added auth_token to users table, working login/register

--- src/backend/app/Http/Controllers/UserController.php
+++ src/backend/app/Http/Controllers/UserController.php
@@ -1,22 +1,27 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 use JWTAuth;
 use JWTAuthException;
 
+
 class UserController extends Controller
 {
+
     private function getToken($email, $password)
     {
         $token = null;
-        //$credentials = $request->only('email', 'password');
+
         try {
             if (!$token = JWTAuth::attempt( ['email'=>$email, 'password'=>$password])) {
                 return response()->json([
                     'response' => 'error',
                     'message' => 'Password or email is invalid',
-                    'token'=>$token
+                    'token' => $token
                 ]);
             }
         } catch (JWTAuthException $e) {
@@ -27,49 +32,79 @@ class UserController extends Controller
         }
         return $token;
     }
+
     public function login(Request $request)
     {
-        $user = \App\User::where('email', $request->email)->get()->first();
-        if ($user && \Hash::check($request->password, $user->password)) // The passwords match...
-        {
+        $user = User::where('email', $request->email)
+            ->get()
+            ->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
             $token = self::getToken($request->email, $request->password);
             $user->auth_token = $token;
             $user->save();
-            $response = ['success'=>true, 'data'=>['id'=>$user->id,'auth_token'=>$user->auth_token,'name'=>$user->name, 'email'=>$user->email]];
+
+            $response = [
+                'success' => true,
+                'data' => [
+                    'id' => $user->id,
+                    'auth_token' => $user->auth_token,
+                    'name' => $user->name,
+                    'email' => $user->email
+                ]
+            ];
+        } else {
+            $response = [
+                'success' => false,
+                'data' => 'Record doesnt exists'
+            ];
         }
-        else
-            $response = ['success'=>false, 'data'=>'Record doesnt exists'];
 
         return response()->json($response, 201);
     }
+
     public function register(Request $request)
     {
         $payload = [
-            'password'=>\Hash::make($request->password),
-            'email'=>$request->email,
-            'name'=>$request->name,
-            'auth_token'=> ''
+            'password' => Hash::make($request->password),
+            'email' =>$request->email,
+            'name' =>$request->name,
+            'auth_token' => ''
         ];
 
         $user = new User($payload);
-        if ($user->save())
-        {
+        if ($user->save()) {
+            $token = self::getToken($request->email, $request->password);
 
-            $token = self::getToken($request->email, $request->password); // generate user token
+            if (!is_string($token)) {
+                return response()->json([
+                    'success' => false,
+                    'data' => 'Token generation failed'
+                ], 201);
+            }
 
-            if (!is_string($token))  return response()->json(['success'=>false,'data'=>'Token generation failed'], 201);
+            $user = User::where('email', $request->email)
+                ->get()
+                ->first();
 
-            $user = User::where('email', $request->email)->get()->first();
-
-            $user->auth_token = $token; // update user token
-
+            $user->auth_token = $token;
             $user->save();
-
-            $response = ['success'=>true, 'data'=>['name'=>$user->name,'id'=>$user->id,'email'=>$request->email,'auth_token'=>$token]];
+            $response = [
+                'success'=>true,
+                'data'=> [
+                    'name'=>$user->name,
+                    'id'=>$user->id,
+                    'email'=>$request->email,
+                    'auth_token'=>$token
+                ]
+            ];
         }
-        else
-            $response = ['success'=>false, 'data'=>'Couldnt register user'];
-
+        else {
+            $response = [
+                'success'=>false,
+                'data'=>'Couldnt register user'
+            ];
+        }
 
         return response()->json($response, 201);
     }
