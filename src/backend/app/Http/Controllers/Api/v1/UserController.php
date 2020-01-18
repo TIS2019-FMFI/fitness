0eef80b22109324cd65d585f5d397a6fe7a9a1b8
Richard <riso.dominik@gmail.com>
2020-01-18 11:55:55 +0100

    enabled jwt blacklist, added methods and routed for logout and token refresh

--- src/backend/app/Http/Controllers/Api/v1/UserController.php
+++ src/backend/app/Http/Controllers/Api/v1/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Requests\Api\v1\User\LoginUserOwn;
 use App\Http\Requests\Api\v1\User\RegisterUserOwn;
 use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use JWTAuth;
@@ -116,4 +117,45 @@ class UserController extends Controller
 
         return response()->json($response, 201);
     }
+
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        auth('api')->logout();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return JsonResponse
+     */
+    public function refresh(): JsonResponse
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     * @return JsonResponse
+     */
+    protected function respondWithToken($token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
 }
